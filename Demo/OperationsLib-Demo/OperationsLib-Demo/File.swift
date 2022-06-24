eb29311cf9c64af976cef5241dd7fffeb6ecb8aa
Ankush Bhatia <ankushbhatia1347@gmail.com>
2022-06-24 23:35:18 +0530

    Adds the Demo Project.

---
+++ Demo/OperationsLib-Demo/OperationsLib-Demo/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ankush Bhatia on 24/06/22.
+//
+
+import Foundation
---
+++ Demo/OperationsLib-Demo/OperationsLib-Demo/TestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TestViewController.swift
+//  
+//
+//  Created by Ankush Bhatia on 24/06/22.
+//
+
+import UIKit
+import OperationsLib
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
