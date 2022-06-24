e906f3f15582b94d5bc6bdb128d8d1059847da1d
Ankush Bhatia <ankushbhatia1347@gmail.com>
2022-06-24 23:38:00 +0530

    Updates the Package.swift.

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OperationsLib",
-            dependencies: [],
-            exclude: ["Demo"]),
+            dependencies: []),
         .testTarget(
             name: "OperationsLibTests",
             dependencies: ["OperationsLib"]),
